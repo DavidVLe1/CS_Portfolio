fcf076f86ad018b5f4f025c5280624b690a78eb8
David Le <le.davidv18@gmail.com>
2023-04-19 22:07:53 -0500

    updated about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,29 +9,7 @@ const About = () => {
 </h7></div>
 <br></br>
 <div><h7>As a result of completing various courses, I have developed a diverse skill set that includes:
-  <ul>
-    <li>Full-stack development using technologies such as Node.js, MongoDB, and Angular.</li>
-    <li>Data visualization techniques for identifying patterns and trends in complex data sets.</li>
-    <li>Understanding of TCP/IP architecture and protocols for communication over the Internet.</li>
-    <li>Knowledge of software engineering methodologies and tools for developing high-quality software systems.</li>
-    <li>Familiarity with cloud computing architecture and deployment models, including SaaS, PaaS, and IaaS.</li>
-    <li>Proficiency in advanced database topics such as data storage, indexing, query processing, and transaction management.</li>
-    <li>Understanding of computer system security principles, including intrusion detection, hardening, and backup procedures.</li>
-    <li>Web development skills encompassing HTML, CSS, PHP, and database integration.</li>
-    <li>Systems analysis and design techniques, including modeling tools such as data flow diagrams and entity-relationship diagrams.</li>
-    <li>Mobile application development, including planning, designing, and building functional apps.</li>
-    <li>Knowledge of modern operating system security implementation for Windows operating systems and applications.</li>
-    <li>These skills highlight my expertise in programming, data analysis, networking, software development, cloud computing, database management, security, web development, systems analysis, and mobile applications.</li>
-  </ul>
-
-
-
-
-
-
-
-
-
+ 
 
 </h7></div>
 </div>
