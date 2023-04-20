a45c813742210191e443753179e1b99f89188df2
David Le <le.davidv18@gmail.com>
2023-04-19 20:47:15 -0500

    updated text in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import "./about.css"
 
 const About = () => {
   return (
-    <div><h7>Hi there! I'm David Le, a software engineer with a Bachelor's degree in Chemistry and work experience as an aquatics 
+    <div><h7>Hi there! I'm David Le, a software engineer with a Master's degree in Computer Science and a bachelors in Chemistry with work experience as an aquatics 
       instructor and lifeguard. In my free time, I love to play video games, draw, and cook. My diverse background has taught me the 
       importance of communication, collaboration, and attention to detail - all skills that are crucial to software engineering. I'm a 
       driven and creative problem-solver who is always seeking new challenges and opportunities for growth. Let's build something great together!</h7></div>
